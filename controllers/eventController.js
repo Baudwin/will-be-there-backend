7597e513d8e5321911872338932e298ba5fc0563
Adewale Toheeb <toheeb.dev1@gmail.com>
2024-04-23 03:45:54 +0100

    fixed the errors and updated some controllers

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -55,7 +55,6 @@ exports.getUserEvent = async (req, res) => {
 		});
 	} catch (error) {
 		res.status(404).json({
-			status: "fail",
 			message: error,
 		});
 	}
@@ -82,7 +81,6 @@ exports.updateEvent = async (req, res) => {
 		});
 	} catch (error) {
 		res.status(400).json({
-			status: "fail",
 			message: "Failed to update event: " + error.message,
 		});
 	}
@@ -91,12 +89,10 @@ exports.deleteEvent = async (req, res) => {
 	try {
 		await Event.findByIdAndDelete(req.params.id);
 		res.status(204).json({
-			status: "success",
 			data: null,
 		});
 	} catch (error) {
 		res.status(404).json({
-			status: "fail",
 			message: error,
 		});
 	}
