2fe60953e295b5574d56e509233f9c7b9bb6a0c9
Adewale Toheeb <toheeb.dev1@gmail.com>
2024-04-24 11:03:14 +0100

    update the security issue

--- cloudinary.js
+++ cloudinary.js
@@ -1,11 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
 require("dotenv").config();
 
-//cloudinary configurration
+//cloudinary configuration
 cloudinary.config({
-	cloud_name: "dtsrvbkrb",
-	api_key: process.env.API_KEY,
-	api_secret: process.env.API_SECRET,
+	cloud_name: process.env.CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
