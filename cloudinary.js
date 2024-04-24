38b4c2d05897223e079b796c3c969df9a0850e09
Adewale Toheeb <toheeb.dev1@gmail.com>
2024-04-24 10:00:39 +0100

    fix the security issues with api key and secret

--- cloudinary.js
+++ cloudinary.js
@@ -3,8 +3,8 @@ import { v2 as cloudinary } from "cloudinary";
 //cloudinary configurration
 cloudinary.config({
 	cloud_name: "dtsrvbkrb",
-	api_key: "746269396443457",
-	api_secret: "KSGinkABtM40FuK1Hl6Ldq7GvMA",
+	api_key: process.env.API_KEY,
+	api_secret: process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
