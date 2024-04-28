274aa88c94e51a4afc19f404c33ea8a59048213f
Titah Baudwin <baudwin12@gmail.com>
2024-04-28 21:42:53 +0100

    debugging vercel deployment

--- supabaseSetup.js
+++ supabaseSetup.js
@@ -1,4 +1,4 @@
-// const  { createClient }  = require('@supabase/supabase-js')
+const createClient   = require('@supabase/supabase-js')
 const supabaseUrl = process.env.supabaseUrl
 const supabaseKey = process.env.SUPABASE_KEY
 // const supabase = createClient(supabaseUrl, supabaseKey)
