a16971490569e4f08ec8f47caa5f4edc2f6fd6a4
Titah Baudwin <baudwin12@gmail.com>
2024-04-28 22:22:31 +0100

    vercel deployment debug

--- supabaseSetup.js
+++ supabaseSetup.js
@@ -1,12 +1,14 @@
-const supabase  = require('@supabase/supabase-js')
 const supabaseUrl = process.env.supabaseUrl
 const supabaseKey = process.env.SUPABASE_KEY
-// const client = supabase.createClient(supabaseUrl, supabaseKey)
+const supabase  = require('@supabase/supabase-js').createClient(supabaseUrl, supabaseKey)
 
+
+// const client = supabase.createClient(supabaseUrl, supabaseKey)
+// console.log(supabase.createClient)
 
 const uploadFunction = async(fileName, fileBuffer, mimetype)=>{
     try {
-        const { data, error } = await client.storage.from('eventimages').upload(fileName,fileBuffer, {
+        const { data, error } = await supabase.storage.from('eventimages').upload(fileName,fileBuffer, {
           contentType: mimetype,
           cacheControl: '3600',
         }); 
