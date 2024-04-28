0ca10416432749a989940418e58ab93e7fa89e86
Titah Baudwin <baudwin12@gmail.com>
2024-04-28 21:10:26 +0100

    debugging vercel deployment

--- routes/eventsRouter.js
+++ routes/eventsRouter.js
@@ -1,21 +1,27 @@
 const express = require("express");
+const {
+	getAllEvents,
+	createEvent,
+	getUserEvent,
+	updateEvent,
+	deleteEvent,
+	// uploadEventImg,
+	getSingleEvent,
+} = require("../controllers/eventController");
+
 const router = express.Router();
-// require('../strategies/JwtStrategy')
-// const {getAllEvents,createEvent,getUserEvent,updateEvent,deleteEvent,getSingleEvent} = require("../controllers/eventController");
+const authenticateJWT = require("../middleware/authenticateJwt");
+
+router.get("/all-events", getAllEvents);
+
+router.post("/create-event",  authenticateJWT, createEvent);
+
+router.get("/my-events",authenticateJWT, getUserEvent);
+
+router.get("/event/:eventId", getSingleEvent);
 
 
-// const authenticateJWT = require("../middleware/authenticateJwt");
-
-// router.get("/all-events", getAllEvents);
-
-// router.post("/create-event", createEvent);
-
-// router.get("/my-events", getUserEvent);
-
-// router.get("/event/:eventId", getSingleEvent);
-
-
-// router.patch("/:eventId", updateEvent);
-// router.delete("/:eventId", deleteEvent);
+router.patch("/:eventId", updateEvent);
+router.delete("/:eventId", deleteEvent);
 
 module.exports = router;
