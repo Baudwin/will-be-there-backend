4e5d9b26a592d212db6c60ab181b7a32c49b0d76
Titah Baudwin <baudwin12@gmail.com>
2024-04-28 21:06:24 +0100

    debugging vercel deployment

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -1,10 +1,9 @@
 const router = require('express').Router()
 const {createRsvp, getUserRsvps}  = require('../controllers/rsvpController')
-require('../strategies/JwtStrategy')
 const authenticateJWT = require("../middleware/authenticateJwt");
 
 
 router.post("/rsvp/:eventId", createRsvp)
-router.get("/my-rsvps", getUserRsvps)
+router.get("/my-rsvps",authenticateJWT, getUserRsvps)
 
 module.exports = router
