342ed74c69ec530893cf6099cbfc90485f009538
Titah Baudwin <baudwin12@gmail.com>
2024-04-28 21:36:51 +0100

    debugging vercel deployment

--- supabaseSetup.js
+++ supabaseSetup.js
@@ -4,17 +4,20 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
-module.exports = async(fileName, fileBuffer, mimetype)=>{
-    try {
-        const { data, error } = await supabase.storage.from('eventimages').upload(fileName,fileBuffer, {
-          contentType: mimetype,
-          cacheControl: '3600',
-        }); 
-        if (error) {
-            throw error
-        }     
-        return data
-      } catch (error) {
-        throw error
-      }
+const uploadFunction = async(fileName, fileBuffer, mimetype)=>{
+    // try {
+    //     const { data, error } = await supabase.storage.from('eventimages').upload(fileName,fileBuffer, {
+    //       contentType: mimetype,
+    //       cacheControl: '3600',
+    //     }); 
+    //     if (error) {
+    //         throw error
+    //     }     
+    //     return data
+    //   } catch (error) {
+    //     throw error
+    //   }
 }
+
+
+module.exports = uploadFunction
