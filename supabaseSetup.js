d2a202e741ba974bc25aaca1825e426679e48370
Titah Baudwin <baudwin12@gmail.com>
2024-04-28 21:57:06 +0100

    debugging vercel deployment- imported the whole supabase package instead of a single function

--- supabaseSetup.js
+++ supabaseSetup.js
@@ -1,22 +1,22 @@
-const createClient   = require('@supabase/supabase-js')
+const supabase  = require('@supabase/supabase-js')
 const supabaseUrl = process.env.supabaseUrl
 const supabaseKey = process.env.SUPABASE_KEY
-// const supabase = createClient(supabaseUrl, supabaseKey)
+const client = supabase.createClient(supabaseUrl, supabaseKey)
 
 
 const uploadFunction = async(fileName, fileBuffer, mimetype)=>{
-    // try {
-    //     const { data, error } = await supabase.storage.from('eventimages').upload(fileName,fileBuffer, {
-    //       contentType: mimetype,
-    //       cacheControl: '3600',
-    //     }); 
-    //     if (error) {
-    //         throw error
-    //     }     
-    //     return data
-    //   } catch (error) {
-    //     throw error
-    //   }
+    try {
+        const { data, error } = await client.storage.from('eventimages').upload(fileName,fileBuffer, {
+          contentType: mimetype,
+          cacheControl: '3600',
+        }); 
+        if (error) {
+            throw error
+        }     
+        return data
+      } catch (error) {
+        throw error
+      }
 }
 
 
