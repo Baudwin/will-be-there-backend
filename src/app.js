b71e4e07d8b0ab7d1e3860d76733bda68dfa283f
Titah Baudwin <baudwin12@gmail.com>
2024-04-29 12:52:50 +0100

    added front end url to cors origin

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-    origin:[ "http://localhost:5178" ],
+    origin:[ "https://will-be-there-xi.vercel.app" ],
     methods:['POST','GET','PUT','DELETE'],
     credentials:true
 }))
